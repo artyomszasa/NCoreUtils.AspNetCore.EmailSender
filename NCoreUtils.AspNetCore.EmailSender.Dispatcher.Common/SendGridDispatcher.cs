5b3ca5d322d728c51fe173956fc19c18ecb7a742
Artyom Antyipin <artyom.szasa@gmail.com>
2026-05-13 08:36:03 +0200

    fix SendGrid auth

--- NCoreUtils.AspNetCore.EmailSender.Dispatcher.Common/SendGridDispatcher.cs
+++ NCoreUtils.AspNetCore.EmailSender.Dispatcher.Common/SendGridDispatcher.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
@@ -38,6 +39,7 @@ public class SendGridDispatcher(
         {
             Content = JsonContent.Create(m, SendGridSerializerContext.Default.SendGridMessage)
         };
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Configuration.ApiKey);
         using var response = await httpClient
             .SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
             .ConfigureAwait(false);
